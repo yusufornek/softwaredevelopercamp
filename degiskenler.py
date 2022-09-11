b4c402504d4b2fb271c3d04981aa542f00203301
Yusuf <43914317+yusufornek@users.noreply.github.com>
2022-09-11 10:20:55 +0000

    Initial commit

---
+++ degiskenler.py
@@ -0,0 +1,15 @@
+baslik = 'İlginizi çekebilir!!!'  #string
+vade = 48 #integer
+faizOrani = 1.69 #float
+
+print('Başlık : ' + baslik)  
+print(vade + vade)  
+print(faizOrani)
+
+#type sadece veri tipi belirtir:
+#operatör
+
+print(type(vade))
+print(type(faizOrani))
+print(type(baslik))
+
