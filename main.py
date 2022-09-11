581172bcff169305ea05d86585c0a9642a3f81c6
Yusuf <43914317+yusufornek@users.noreply.github.com>
2022-09-11 12:08:33 +0000

    latest changes

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 dolarDun = 7.35
+dolarBugun = 7.35
+
+#Syntax
+#Indentation
+#True/False
+
+if dolarDun>dolarBugun:
+  print("Azalış Oku")
+elif dolarDun<dolarBugun:
+  print("Artış Oku")
+else:
+  print("Değişmedi")
+
+  
+  
+  
